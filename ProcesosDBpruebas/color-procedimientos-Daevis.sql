13f7501c1c2565c4042fd5916a9b4784c6764048
fsociety6996 <fsociety6996@gmail.com>
2017-05-11 16:20:21 -0400

    precedimientos_OMTCE-Daevis

---
+++ ProcesosDBpruebas/color-procedimientos-Daevis.sql
@@ -0,0 +1,42 @@
+create function addcolor(
+  pk varchar(15),
+  c1 varchar(15),
+  c2 int
+) returns void as
+$body$
+begin
+insert into color values (pk, c1, c2);
+end;
+$body$
+language plpgsql;
+
+create function editcolor(
+  pk varchar(15),
+  c1 varchar(15),
+) returns void as
+$body$
+begin
+update color set descripcion = c1 where codcolor = pk and codestado != 0;
+end;
+$body$
+language plpgsql;
+
+create function deletecolor(
+  pk varchar(15),
+) returns void as
+$body$
+begin
+update color set codestado = 0 where codcolor = pk;
+end;
+$body$
+language plpgsql;
+
+create function searchcolor(
+  pk varchar(15),
+) returns void as
+$body$
+begin
+select * from color where codcolor = pk and codestado != 0;
+end;
+$body$
+language plpgsql;
