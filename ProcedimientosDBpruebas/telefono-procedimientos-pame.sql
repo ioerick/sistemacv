c98a2493d8b11e92d6ba98889e614b93032d41ae
pamelaescobar <dafne777v@gmail.com>
2017-05-14 17:21:48 -0400

    Add files via upload

---
+++ ProcedimientosDBpruebas/telefono-procedimientos-pame.sql
@@ -0,0 +1,41 @@
+create function addtelefono(
+  pk varchar(15),
+  c1 varchar(15)
+) returns void as
+$body$
+begin
+insert into telefono values (pk, c1, 1);
+end;
+$body$
+language plpgsql;
+
+create function edittelefono(
+  pk varchar(15),
+  c1 varchar(15)
+) returns void as
+$body$
+begin
+update telefono set descripcion = c1 where codtelefono = pk and codestado != 0;
+end;
+$body$
+language plpgsql;
+
+create function deletetelfono(
+  pk varchar(15)
+) returns void as
+$body$
+begin
+update sexo set codestado = 0 where codtelefono = pk and codestado != 0;
+end;
+$body$
+language plpgsql;
+
+create function searchtelefono(
+  pk varchar(15)
+) returns void as
+$body$
+begin
+select * from telefono where codtelefono = pk and codestado != 0;
+end;
+$body$
+language plpgsql;
