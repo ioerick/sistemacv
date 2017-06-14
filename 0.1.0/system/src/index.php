8d577498dab50149c7a7b03513aa7e47264194a7
ioerick <micorreo.erick@gmail.com>
2017-06-14 14:50:38 -0400

    modulo conexcion

--- 0.1.0/system/src/index.php
+++ 0.1.0/system/src/index.php
@@ -1,13 +1,8 @@
 <?php
-    $host="localhost";
-    $port="5432";
-    $user="postgres";
-    $pass="io_passw0rd";
-    $dbname="dbsistemacv";
-    $connect = pg_connect("host=$host, port=$port, user=$user, pass=$pass, dbname=$dbname");
-    if(!$connect)
-        echo "<p><i>No me conecte</i></p>";
-    else
-        echo "<p><i>Me conecte</i></p>";
-    pg_close($connect);
+    // $host="localhost";
+    // $port="5432";
+    // $user="postgres";
+    // $pass="io_passw0rd";
+    // $dbname="dbsistemacv";
+    pg_connect("host=localhost, port=5432, user=postgres, pass=io_passw0rd, dbname=dbsistemacv") or die ("no conecta" . pg_last_error());
 ?>
